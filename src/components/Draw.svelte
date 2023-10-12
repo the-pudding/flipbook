e5b7c297fcf9e45912239ec4e5915682ddf69f5c
russellsamora <russellsamora@gmail.com>
2023-10-12 13:29:29 -0400

    more

--- src/components/Draw.svelte
+++ src/components/Draw.svelte
@@ -6,7 +6,9 @@
 </script>
 
 <p>
-	{@html copy.task}
+	<!-- {@html copy.task} -->
+	Your task is to trace the
+	<mark><strong>last person’s drawing</strong></mark> as best you can.
 </p>
 
 <Canvas />
@@ -15,5 +17,11 @@
 	p {
 		max-width: 300px;
 		margin: 16px auto;
+	}
+
+	mark {
+		background: none;
+		border-bottom: 4px solid rgba(255, 0, 0, 0.5);
+		padding: 0;
 	}
 </style>
