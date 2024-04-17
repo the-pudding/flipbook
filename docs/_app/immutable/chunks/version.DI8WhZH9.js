22fd771464ab1a26eb275fa3731a590fcc2cffae
Russell Samora <russellsamora@gmail.com>
2024-04-17 13:24:38 -0400

    update github pages

--- docs/_app/immutable/chunks/version.DI8WhZH9.js
+++
@@ -1,1 +0,0 @@
-function o(){console.log("--- --- --- --- --- ---"),console.log("svelte-starter: 5.13.0"),console.log("build: 2024-04-17-13:04"),console.log("--- --- --- --- --- ---")}export{o as v};
--- docs/_app/immutable/entry/start.BE0Cn7X1.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.GNYTf1Xc.js";export{t as start};
